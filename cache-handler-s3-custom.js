0bc4f24a905f4f24085a487e6c520b4f4e1f12a0
ryota-09 <n.ryo950911@gmail.com>
2024-01-03 14:16:06 +0900

    カスタムcache-handlerの作成

--- cache-handler-s3-custom.js
+++ cache-handler-s3-custom.js
@@ -8,33 +8,42 @@ AWS.config.update({
   region: "ap-northeast-1",
 });
 
+const bucketName = 'cache-handler';
+
 // S3クライアントを作成する
 const s3 = new AWS.S3();
 
 // キャッシュをS3に保存する関数
-function saveToS3(key, data) {
-  const params = {
-    Bucket: 'your-bucket-name',
-    Key: key,
-    Body: data
-  };
-
-  return s3.putObject(params).promise();
+async function saveToS3(key, data) {
+  try {
+    const params = {
+      Bucket: bucketName,
+      Key: key,
+      Body: value,
+    };
+    await s3.putObject(params).promise();
+  } catch (error) {
+    console.error('Error setting cache in S3:', error);
+  }
 }
 
 // キャッシュをS3から取得する関数
-function getFromS3(key) {
-  const params = {
-    Bucket: 'your-bucket-name',
-    Key: key
-  };
-
-  return s3.getObject(params).promise()
-    .then(response => response.Body.toString());
+async function getCache(key) {
+  try {
+    const params = {
+      Bucket: bucketName,
+      Key: key,
+    };
+    const data = await s3.getObject(params).promise();
+    return data.Body.toString('utf-8');
+  } catch (error) {
+    console.error('Error getting cache from S3:', error);
+    return null;
+  }
 }
 
 // エクスポートする
 module.exports = {
   saveToS3,
-  getFromS3
+  getCache
 };
