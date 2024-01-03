8923aeee5c6424352909c6f06bda7f35bcb51c4c
ryota-09 <n.ryo950911@gmail.com>
2024-01-03 13:52:53 +0900

    first commit🎉

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  experimental: {
+    incrementalCacheHandlerPath: require.resolve('./cache-handler-s3-custom.js'),
+  },
+};
 
 module.exports = nextConfig;
